9282d09feab04fa9b470f0e8f7bc2b7a0fd2ecd6
Sergio Espinoza <sergio.espinoza.lopez@gmail.com>
2020-05-23 16:18:49 -0500

    bull vertical screener - work in progress

--- getSp500Constituents.py
+++ getSp500Constituents.py
@@ -1,25 +1,23 @@
 from datapackage import Package
 import pandas as pd
 
-package = Package('https://datahub.io/core/s-and-p-500-companies/datapackage.json')
+package = Package('https://datahub.io/core/s-and-p-500-companies-financials/datapackage.json')
+
+df = pd.DataFrame()
 
 # print list of all resources:
 print(package.resource_names)
 
-csv_str = ''
-
-
-
 # print processed tabular data (if exists any)
 for resource in package.resources:
     if resource.descriptor['datahub']['type'] == 'derived/csv' and \
-    resource.descriptor['name'] == 'constituents_csv':
-        data_sp500 = resource.read()
-        df = pd.DataFrame(data=data_sp500, columns=['symbol', 'name', 'sector'])
+    resource.descriptor['name'] == 'constituents-financials_csv':
+        data_sp500 = resource.read( keyed=True)
+        print( type(data_sp500) )
+        print( data_sp500[:10] )
+        df = pd.DataFrame(data=data_sp500)
         print(df.head())
         break
 
-
-
-if len(df['symbol'] ) > 0:
+if len(df['Symbol'] ) > 0:
     df.to_csv('sp500Constituents.csv')
